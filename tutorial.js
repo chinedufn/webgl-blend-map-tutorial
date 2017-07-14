cd65d47735e12fe0525512006cfe45f5a4724c47
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-07-14 22:25:13 +0900

    Add undo

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,10 @@
+/**
+ * Section 1: Creating our paintbrush controls
+ */
+
+// Create the canvas that we will be using our paintbrush on.
+// The contents of this canvas will become to blend map that
+// we use to multitexture our WebGL plane.
 var drawCanvas = document.createElement('canvas')
 drawCanvas.width = 256
 drawCanvas.height = 256
@@ -5,11 +12,13 @@ drawCanvas.style.width = '256px'
 drawCanvas.style.height = '256px'
 drawCanvas.style.border = 'solid 1px rgba(0, 0, 0, 0.1)'
 
-var sliderContainer = document.createElement('div')
+// We start our paintbrush
+var brushColor = 'purple'
 
-function createColorSlider (startVal) {
+// Create the sliders that allow you to change the
+// red, green and blue components of the paintbrush color
+function createColorSlider () {
   var colorSlider = document.createElement('input')
-  colorSlider.value = startVal
   colorSlider.type = 'range'
   colorSlider.min = 0
   colorSlider.max = 255
@@ -18,62 +27,67 @@ function createColorSlider (startVal) {
 
   return colorSlider
 }
-function createSliderWrapper (label, colorSlider) {
+var redSlider = createColorSlider()
+var blueSlider = createColorSlider()
+var greenSlider = createColorSlider()
+
+// Whenever our sliders are slid we update the brush color
+function handleSliderChange () {
+  brushColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`
+  setColorDisplays()
+}
+
+// Wrap the sliders in a div that has a label
+// of R, G or B
+function addSliderLabel (label, colorSlider) {
   var sliderLabel = document.createElement('label')
   sliderLabel.display = 'inline-block'
   sliderLabel.innerHTML = label
 
   var sliderWrapper = document.createElement('div')
+  sliderWrapper.display = 'block'
   sliderWrapper.appendChild(sliderLabel)
   sliderWrapper.appendChild(colorSlider)
-
-  sliderWrapper.display = 'block'
 
   return sliderWrapper
 }
 
-var redSlider = createColorSlider(150)
-var blueSlider = createColorSlider(200)
-var greenSlider = createColorSlider(10)
+// Create a div to hold all of our sliders
+var sliderContainer = document.createElement('div')
+sliderContainer.appendChild(addSliderLabel('R', redSlider))
+sliderContainer.appendChild(addSliderLabel('G', greenSlider))
+sliderContainer.appendChild(addSliderLabel('B', blueSlider))
 
-var currentColor = 'purple'
-
-function handleSliderChange () {
-  currentColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`
-  redDisplay.innerHTML = 'rgb(' + redSlider.value + ', '
-  greenDisplay.innerHTML = blueSlider.value + ', '
-  blueDisplay.innerHTML = greenSlider.value + ')'
-  swatchDisplay.style.backgroundColor = currentColor
+// Reset the display that shows what the current brush color is
+function setColorDisplays () {
+  colorDisplay.innerHTML = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`
+  swatchDisplay.style.backgroundColor = brushColor
 }
-sliderContainer.appendChild(createSliderWrapper('R', redSlider))
-sliderContainer.appendChild(createSliderWrapper('G', greenSlider))
-sliderContainer.appendChild(createSliderWrapper('B', blueSlider))
 
-var redDisplay = document.createElement('span')
-redDisplay.innerHTML = 'rgb(' + redSlider.value + ','
-var greenDisplay = document.createElement('span')
-greenDisplay.innerHTML = greenSlider.value + ','
-var blueDisplay = document.createElement('span')
-blueDisplay.innerHTML = blueSlider.value + ')'
+// Create the displays that show what the current color is
+var colorDisplay = document.createElement('span')
 var swatchDisplay = document.createElement('div')
-swatchDisplay.style.backgroundColor = 'purple'
 swatchDisplay.style.width = '20px'
 swatchDisplay.style.height = '20px'
 swatchDisplay.style.marginRight = '5px'
+swatchDisplay.style.borderRadius = '50%'
+setColorDisplays()
 
-var colorContainer = document.createElement('div')
-colorContainer.style.display = 'flex'
-colorContainer.style.alignItems = 'center'
-colorContainer.appendChild(swatchDisplay)
-colorContainer.appendChild(redDisplay)
-colorContainer.appendChild(greenDisplay)
-colorContainer.appendChild(blueDisplay)
+// Create the container that holds our brush's color display
+var colorDisplayContainer = document.createElement('div')
+colorDisplayContainer.style.display = 'flex'
+colorDisplayContainer.style.alignItems = 'center'
+colorDisplayContainer.appendChild(swatchDisplay)
+colorDisplayContainer.appendChild(colorDisplay)
 
+// Create the preset buttons to select the appropriate color
+// for painting one of our four textures
 var buttonContainer = document.createElement('div')
 buttonContainer.style.display = 'flex'
 buttonContainer.style.width = '300px'
 buttonContainer.style.flexWrap = 'wrap'
 
+// Create one of our preset buttons
 function createColorButton (color, label) {
   var colorButton = document.createElement('button')
 
@@ -84,164 +98,242 @@ function createColorButton (color, label) {
   colorButton.style.marginRight = '5px'
   colorButton.innerHTML = label
   colorButton.onclick = function () {
-    currentColor = `rgb(${color.join(',')})`
-    redDisplay.innerHTML = 'rgb(' + color[0] + ', '
-    greenDisplay.innerHTML = color[1] + ', '
-    blueDisplay.innerHTML = color[2] + ')'
+    brushColor = `rgb(${color.join(',')})`
     redSlider.value = color[0]
     greenSlider.value = color[1]
     blueSlider.value = color[2]
-    swatchDisplay.style.backgroundColor = currentColor
+    setColorDisplays()
   }
 
   buttonContainer.appendChild(colorButton)
 }
-
+// Create our four preset buttons. Our fragment will
+// read these colors from the blend map in order to know
+// which textures to sample
+createColorButton([0, 0, 0], 'stone')
 createColorButton([255, 0, 0], 'lava')
 createColorButton([0, 255, 0], 'moss')
 createColorButton([0, 0, 255], 'water')
-createColorButton([0, 0, 0], 'stone')
 
+// Add an undo button
+var undoButton = document.createElement('button')
+undoButton.style.cursor = 'pointer'
+undoButton.innerHTML = 'Undo'
+undoButton.onclick = undo
+
+// Add all of our color controls into the page
 var mountElem = document.querySelector('#webgl-blend-map-tutorial') || document.body
+mountElem.appendChild(undoButton)
 mountElem.appendChild(buttonContainer)
 mountElem.appendChild(sliderContainer)
-mountElem.appendChild(colorContainer)
+mountElem.appendChild(colorDisplayContainer)
 
+// Create the canvas that will hold our WebGL terrain
 var webGLCanvas = document.createElement('canvas')
 webGLCanvas.width = 512
 webGLCanvas.height = 512
 mountElem.appendChild(webGLCanvas)
 
+// Add our paintbrush canvas and WebGL canvas into the page
 var canvasContainer = document.createElement('div')
 canvasContainer.style.display = 'flex'
 canvasContainer.appendChild(drawCanvas)
 canvasContainer.appendChild(webGLCanvas)
 mountElem.appendChild(canvasContainer)
 
-var context = drawCanvas.getContext('2d')
-context.fillStyle = currentColor
-context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+/**
+ * Section 2 - Painting to our blendmap
+ */
 
-var blendmapImage = new window.Image()
-blendmapImage.src = drawCanvas.toDataURL()
+// Get our 2d context for our painting canvas. This allows
+// us to draw onto our 2d canvas
+var canvas2dContext = drawCanvas.getContext('2d')
+canvas2dContext.fillStyle = brushColor
+canvas2dContext.fillRect(0, 0, canvas2dContext.canvas.width, canvas2dContext.canvas.height)
 
-var terrainImage = new window.Image()
-
+// A flag to let us know whether or not we should draw onto our canvas when the mouse
+// moves over the canvas
 var isPainting = false
 
+// When the user clicks or touches the canvas we begin painting to our canvas
 function startPainting (e) {
   isPainting = true
   addPoint(
       e.pageX - drawCanvas.offsetLeft,
       e.pageY - drawCanvas.offsetTop,
+      // This indicates not to connect this new paint with the
+      // previous blob of paint
       false
   )
   repaint()
 }
 
+// Add a new point to our canvas and connect it with the previous point.
+// This happens when you're dragging your mouse / finger
 function movePaintbrush (e) {
   if (isPainting) {
     addPoint(
       (e.pageX || e.changedTouches[0].pageX) - drawCanvas.offsetLeft,
       (e.pageY || e.changedTouches[0].pageY) - drawCanvas.offsetTop,
+      // This indicates to connect this blob of paint with
+      // the previous blob of paint
       true
     )
     repaint()
   }
 }
 
+// When the user lifts their finger or mouse we stop painting
+// We keep track of the last point that they drew before release
+// their mouse so that our undo button can revert us back to that
+// point
+var lastMouseReleaseIndices = []
+function stopPainting () {
+  lastMouseReleaseIndices.push(allPoints.length - 1)
+  isPainting = false
+}
+
+// Add event listeners to draw when we mouse / touching the canvas
 drawCanvas.addEventListener('mousedown', startPainting)
 drawCanvas.addEventListener('touchstart', startPainting)
-
 drawCanvas.addEventListener('mousemove', movePaintbrush)
 drawCanvas.addEventListener('touchmove', movePaintbrush)
+drawCanvas.addEventListener('mouseup', stopPainting)
+drawCanvas.addEventListener('touchend', stopPainting)
 
-drawCanvas.addEventListener('mouseup', function (e) {
-  isPainting = false
-})
-drawCanvas.addEventListener('touchend', function (e) {
-  isPainting = false
-})
-
+// Keep track of everytime the user adds a new blob of paint.
+// When we repaint our canvas we go through all of the blobs
+// of paint that they added and redraw to the canvas at
+// those points.
 var allPoints = []
 function addPoint (x, y, connectWithPrevious) {
   allPoints.push({
     x: x,
     y: y,
     connect: connectWithPrevious,
-    color: currentColor
+    color: brushColor
   })
 }
 
-function repaint () {
-  context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-
-  context.lineJoin = 'round'
-  context.lineWidth = 20
-
-  for (var i = 0; i < allPoints.length; i++) {
-    context.beginPath()
-    if (allPoints[i].connect) {
-      context.moveTo(allPoints[i - 1].x, allPoints[i - 1].y)
-    } else {
-      context.moveTo(allPoints[i].x - 1, allPoints[i].y)
-    }
-    context.lineTo(allPoints[i].x, allPoints[i].y)
-
-    context.closePath()
-
-    context.strokeStyle = allPoints[i].color
-    context.stroke()
+// To undo a paint stroke we remove the last element in our array of
+// paint points
+function undo () {
+  if (lastMouseReleaseIndices.length > 1) {
+    allPoints = allPoints.slice(
+      0,
+      lastMouseReleaseIndices[lastMouseReleaseIndices.length - 2]
+    )
+  } else if (lastMouseReleaseIndices.length === 1) {
+    allPoints = []
   }
 
+  lastMouseReleaseIndices.pop()
+  repaint()
+}
+
+// Loop through all of the points in our array of paint strokes
+// and use that to re-draw our canvas
+function repaint () {
+  // Start by clearing our canvas
+  canvas2dContext.fillRect(0, 0, canvas2dContext.canvas.width, canvas2dContext.canvas.height)
+
+  // Set the line type and size
+  canvas2dContext.lineJoin = 'round'
+  canvas2dContext.lineWidth = 20
+
+  for (var i = 0; i < allPoints.length; i++) {
+    canvas2dContext.beginPath()
+    if (allPoints[i].connect && allPoints[i - 1]) {
+      // If this is a connected brush stroke we connect it with
+      // the previous brush stroke
+      canvas2dContext.moveTo(allPoints[i - 1].x, allPoints[i - 1].y)
+    } else {
+      // If this is just a click we draw a 1 pixel wide stroke
+      // right where they clicked
+      canvas2dContext.moveTo(allPoints[i].x - 1, allPoints[i].y)
+    }
+
+    // Draw our paint line
+    canvas2dContext.lineTo(allPoints[i].x, allPoints[i].y)
+    canvas2dContext.closePath()
+    canvas2dContext.strokeStyle = allPoints[i].color
+    canvas2dContext.stroke()
+  }
+
+  // Make our WebGL canvas's blend map reload itself from the contents of our canvas
   blendmapImage.src = drawCanvas.toDataURL()
+  // Update our WebGL canvas's blendmap texture with this new 2d paint canvas image
   blendmapImage.onload = function () {
     gl.bindTexture(gl.TEXTURE_2D, blendmapTexture)
     gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, blendmapImage)
-    // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, blendmapImage)
     gl.bindTexture(gl.TEXTURE_2D, null)
   }
 }
 
 /**
- * WebGL Canvas Code
+ * Section 3: Drawing our WebGL terrain
  */
+
+// Get our WebGL context. We'll use this in order to
+// draw onto our WebGL canvas
 var gl = webGLCanvas.getContext('webgl')
 gl.clearColor(0.0, 0.0, 0.0, 1.0)
 
+// Create WebGLBuffers for our vertices. We will buffer their
+// positions, the other to draw them, and the coordinates in our
+// texture (uvs) that each vertex should use
 var vertexPositionBuffer = gl.createBuffer()
 var vertexIndexBuffer = gl.createBuffer()
 var vertexUVsBuffer = gl.createBuffer()
 
+// We're going to generate a terrain. A terrain is just
+// a bunch of triangles in a grid. The points on the triangles have different
+// heights. To make this tutorial a little simpler, we're just going to make
+// our "terrain" completely flat. Otherwise we'd need to add in some code to
+// handle normals and lighting
 var positions = []
 var indices = []
 var uvs = []
-var distance = -85
+
+// The distance into the scene that we're placing the terrain
+var distanceAway = -85
+
+// Keep track of which tile we are currently rendering. Each tile
+// is a square made up of two triangles. Knowing which tile number
+// we are on lets us know where to put this square
 var tileNum = 0
-for (var y = 0; y < 64; y++) {
-  for (var x = 0; x < 64; x++) {
+
+// Generate our vertex data for the rows and columns
+// of our terrain grid
+var numRowsColumns = 64
+for (var y = 0; y < numRowsColumns; y++) {
+  for (var x = 0; x < numRowsColumns; x++) {
     positions = positions.concat([
       // Bottom left tile corner
-      x, y, distance,
+      x, y, distanceAway,
       // Bottom right tile corner
-      1 + x, y, distance,
+      1 + x, y, distanceAway,
       // Top right tile corner
-      1 + x, 1 + y, distance,
+      1 + x, 1 + y, distanceAway,
       // Top left tile corner
-      x, 1 + y, distance
+      x, 1 + y, distanceAway
     ])
 
+    // The bottom left of our grid will use [0, 0] of
+    // our blend map uvs. The top right of our grid
+    // will use [1, 1] of our blend map uvs
     uvs = uvs.concat([
       // Bottom left tile corner
-      x / 64, y / 64,
+      x / numRowsColumns, y / numRowsColumns,
       // Bottom right tile corner
-      (x + 1) / 64, y / 64,
+      (x + 1) / numRowsColumns, y / numRowsColumns,
       // Top right tile corner
-      (x + 1) / 64, (y + 1) / 64,
+      (x + 1) / numRowsColumns, (y + 1) / numRowsColumns,
       // Top left tile corner
-      x / 64, (y + 1) / 64
+      x / numRowsColumns, (y + 1) / numRowsColumns
     ])
 
+    // These are used by gl.drawElements to draw our grid
     indices = indices.concat([
       0, 1, 2, 0, 2, 3
     ].map(function (num) {
@@ -276,18 +368,21 @@ function handleLoadedTexture (texture, image) {
   gl.bindTexture(gl.TEXTURE_2D, null)
 
   if (bothImagesLoaded) {
-    setUpWebGLState()
+    setupWebGLState()
   }
   bothImagesLoaded = true
 }
 
 var blendmapTexture = gl.createTexture()
+var blendmapImage = new window.Image()
+blendmapImage.src = drawCanvas.toDataURL()
 blendmapImage.crossOrigin = 'anonymous'
 blendmapImage.onload = function () {
   handleLoadedTexture(blendmapTexture, blendmapImage)
 }
 
 var terrainTexture = gl.createTexture()
+var terrainImage = new window.Image()
 terrainImage.crossOrigin = 'anonymous'
 terrainImage.onload = function () {
   handleLoadedTexture(terrainTexture, terrainImage)
@@ -354,7 +449,7 @@ gl.linkProgram(shaderProgram)
 
 gl.useProgram(shaderProgram)
 
-function setUpWebGLState () {
+function setupWebGLState () {
   var vertexPositionAttribute = gl.getAttribLocation(shaderProgram, 'aVertexPosition')
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer)
   gl.enableVertexAttribArray(vertexPositionAttribute)
@@ -385,7 +480,7 @@ function setUpWebGLState () {
 
 // TODO: Use create-draw-function. Write unit tests two accept two textures
 function drawWebGLCanvas () {
-  setUpWebGLState()
+  setupWebGLState()
   if (bothImagesLoaded) {
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.viewport(0, 0, 512, 512)
